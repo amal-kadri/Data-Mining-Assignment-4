3b770ef6c47466a3e8caf7f755337a69b1a736b4
amal-kadri <98130059+amal-kadri@users.noreply.github.com>
2022-04-20 16:58:16 -0500

    misc PCA stuff

--- code/2_marketseg.R
+++ code/2_marketseg.R
@@ -16,7 +16,11 @@ clust1 = kmeans(market_noID, 10, nstart = 25) #chose 10 after some trial and err
 clust1$center %>% round(1)  # not super helpful, location in a 9 dimensional feature space
 clusterID = clust1$cluster
 
-social_marketing = cbind(social_marketing,clusterID)
+rownames(social_marketing) <- social_marketing$`...1`
+social_clustered = cbind(social_marketing,clusterID) %>%
+  mutate(`...1` = NULL)
+
+
 
 # A few plots with cluster membership shown
 # qplot is in the ggplot2 library
@@ -24,3 +28,7 @@ qplot(current_events, sports_fandom, data=market_noID, color=factor(clust1$clust
 qplot(religion, sports_fandom, data=market_noID, color=factor(clust1$cluster))
 qplot(religion, sports_fandom, data=market_noID, color=factor(clust1$cluster))
 centroids = as.data.frame(clust1[["centers"]])
+df = social_clustered[,(1:36)]
+market_PCA = prcomp(social_clustered[,(1:36)], scale=FALSE, rank=2)
+plot(market_PCA)
+round(market_PCA$rotation[,1:2],2)
