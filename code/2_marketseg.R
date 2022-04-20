11e141caae41ef3011f72ff0ad0e2087f7977c2c
amal-kadri <98130059+amal-kadri@users.noreply.github.com>
2022-04-20 18:38:35 -0500

    messing around with PCA

--- code/2_marketseg.R
+++ code/2_marketseg.R
@@ -29,6 +29,27 @@ qplot(religion, sports_fandom, data=market_noID, color=factor(clust1$cluster))
 qplot(religion, sports_fandom, data=market_noID, color=factor(clust1$cluster))
 centroids = as.data.frame(clust1[["centers"]])
 df = social_clustered[,(1:36)]
-market_PCA = prcomp(social_clustered[,(1:36)], scale=FALSE, rank=2)
+
+
+market_PCA = prcomp(social_clustered[,(1:36)], scale=FALSE, rank=10)
+round(market_PCA$rotation[,1:10],2)
 plot(market_PCA)
-round(market_PCA$rotation[,1:2],2)
+df = as.data.frame(round(market_PCA$rotation[,1:10],2))
+
+# shows = merge(shows, PCApilot$x[,1:3], by="row.names")
+# shows = rename(shows, Show = Row.names)
+
+categorized_data = social_marketing %>%
+  select(-`...1`)
+categorized_data =  as.data.frame(t(categorized_data))
+market_PCA2 = prcomp(categorized_data[,(1:36)], scale=FALSE, rank=5)
+round(market_PCA2$rotation[,1:5],2)
+  
+  group_by(Show) %>% 
+  select(-Viewer) %>%
+  summarize_all(mean) %>%
+  column_to_rownames(var="Show") 
+  
+
+
+
