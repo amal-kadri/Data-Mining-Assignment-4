745719f2b0a6009573765190d0b0ea08af7d0a2a
dascolari <dascolari@utexas.edu>
2022-04-22 21:57:56 -0500

    oan

--- code/oan_the_lib.R
+++ code/oan_the_lib.R
@@ -4,15 +4,12 @@ library(tidyverse)
 library(foreach)
 library(arules)
 library(arulesViz)
-<<<<<<< Updated upstream
 library(ggplot2)
 library(LICORS)  # for kmeans++
 library(foreach)
 library(mosaic)
 library(foreach)
-=======
 library(iterators)
->>>>>>> Stashed changes
 
 
 path <- here()
